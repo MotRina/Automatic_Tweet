512f8ae60fe10d7b8e99092d4827babf52bd2711
Rina Motoyama <127751292+MotRina@users.noreply.github.com>
2024-02-02 00:15:25 +0900

    Create twitter_api.py

---
+++ twitter_api.py
@@ -0,0 +1,20 @@
+import tweepy
+import os
+
+# XのAPIで以下を事前に取得
+consumerKey = os.environ["TWITTER_CONSUMER_KEY"]
+consumerSecret = os.environ["TWITTER_CONSUMER_SECRET"]
+accessToken = os.environ["TWITTER_ACCESS_TOKEN"]
+accessTokenSecret = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
+bearerToken = os.environ["TWITTER_BEARER_TOKEN"]
+
+def post(tweet):
+    client = tweepy.Client(
+        bearerToken,
+        consumerKey,
+        consumerSecret,
+        accessToken,
+        accessTokenSecret
+    )
+
+    client.create_tweet(text=tweet)
